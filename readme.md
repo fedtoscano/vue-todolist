74b1a9eed35dfa72377b9db976b7acbd1fb78846
fedtoscano <federicotoscano611@gmail.com>
2024-05-30 14:08:24 +0200

    creato array di oggetti e renderizzato correttamente nell-HTML

--- readme.md
+++ readme.md
@@ -18,3 +18,6 @@ Bonus:
 
 
 
+1. creo il data object con le proprieta' della task -> todoList
+    1.1 nell-HTML creo un v-for con un article per ogni elemento dell-array todoList
+    
--- script/script.js
+++ script/script.js
@@ -2,7 +2,29 @@ const { createApp } = Vue
 
 createApp({
     data() {
-    return {}
+    return {
+        todoList:[{
+            text:'Andare a fare la spesa',
+            done: false,
+        },
+        {
+            text:'Giocare a scacchi',
+            done: false,
+        },
+        {
+            text:'Ascoltare Amnesiac dei Radiohead',
+            done: false,
+        },
+        {
+            text:'Comprare le uova',
+            done: false,
+        },
+        {
+            text:'Comprare il pane',
+            done: false,
+        },
+    ]
+    }
     },
     methods: {}
 }).mount('#app')
